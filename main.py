da8c9602b4fa9cd5ce419eda9db56e6c857d22b0
titan1540 <titan1540@mail.ru>
2019-04-29 13:46:34 +0300

    Task 3

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import os
-import sys
-import requests
 import pygame
 
 import mapapi
@@ -73,7 +71,14 @@ class MapImage(pygame.sprite.Sprite):
         self.spn = None
         self.ll = None
 
-    def update_map(self, event=None):
+    def new_map(self):
+        coordinates = input_label.get_text()
+        toponym = geocoder.get_toponym(coordinates)
+        self.spn = geocoder.get_spn(toponym)
+        self.ll = geocoder.get_ll(toponym)
+        self.update_map()
+
+    def update_spn(self, event=None):
         if event is not None:
             if event.key == pygame.K_PAGEUP:
                 if self.spn[0] * 2 <= 90 and self.spn[1] * 2 <= 90:
@@ -81,23 +86,36 @@ class MapImage(pygame.sprite.Sprite):
             else:
                 if self.spn[0] / 2 >= 0.001 and self.spn[1] / 2 >= 0.001:
                     self.spn = self.spn[0] / 2, self.spn[1] / 2
-            mapapi.save_map(self.ll, self.spn)
+        self.update_map()
 
+    def update_map(self):
+        mapapi.save_map(self.ll, self.spn)
         self.image = load_image('_map.png')
 
-    def new_map(self):
-        coordinates = input_label[0].get_text()
-        toponym = geocoder.get_toponym(coordinates)
-        self.spn = geocoder.get_spn(toponym)
-        self.ll = geocoder.get_ll(toponym)
-        mapapi.save_map(self.ll, self.spn)
+    def move_map(self, event):
+        if event.key == pygame.K_RIGHT:
+            if self.ll[0] + self.spn[0] < 180:
+                self.ll = self.ll[0] + self.spn[0], self.ll[1]
+        if event.key == pygame.K_LEFT:
+            if self.ll[0] - self.spn[0] > -180:
+                self.ll = self.ll[0] - self.spn[0], self.ll[1]
+        if event.key == pygame.K_UP:
+            if self.ll[1] + self.spn[1] < 90:
+                self.ll = self.ll[0], self.ll[1] + self.spn[1]
+        if event.key == pygame.K_DOWN:
+            if self.ll[1] - self.spn[1] > -90:
+                self.ll = self.ll[0], self.ll[1] - self.spn[1]
+
         self.update_map()
 
 
-input_label = (InputLabel(15, 15, 100, 20), Border(10, 10, 300, 30))
+input_label = InputLabel(15, 15, 100, 20)
+Border(10, 10, 300, 30)
 
-map_img = (MapImage(), Border(10, 50, 600, 450))
-btn_search = (Button(330, 15, 100, 20, 'Найти', map_img[0].new_map), Border(320, 10, 80, 30))
+map_img = MapImage()
+Border(10, 50, 600, 450)
+btn_search = Button(330, 15, 100, 20, 'Найти', map_img.new_map)
+Border(320, 10, 80, 30)
 
 
 def load_image(name, colorkey=None):
@@ -126,9 +144,13 @@ while running:
             running = False
         elif event.type == pygame.KEYDOWN and \
                 (event.key == pygame.K_PAGEUP or event.key == pygame.K_PAGEDOWN):
-            map_img[0].update_map(event)
+            map_img.update_spn(event)
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+            btn_search.action()
+        elif event.type == pygame.KEYDOWN and 273 <= event.key <= 276:
+            map_img.move_map(event)
         elif event.type == pygame.KEYDOWN:
-            input_label[0].push_button(event)
+            input_label.push_button(event)
         elif event.type == pygame.MOUSEBUTTONDOWN:
             buttons.update(event)
 
@@ -138,5 +160,5 @@ while running:
 pygame.quit()
 try:
     os.remove('_map.png')
-except:
+except Exception:
     pass
